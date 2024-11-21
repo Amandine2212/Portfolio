d27672232ed7a7b9bfd76b839a432141b1dd9ccf
Amandine2212 <amandinelarrosa@hotmail.fr>
2024-11-21 15:24:18 +0100

    Add files via upload

---
+++ CSS/script.js
@@ -0,0 +1,17 @@
+// formulaire
+let form = document.querySelector('Form')
+
+form.addEventListener("button"),(e)=>{
+
+// bloquer l'envoi du formulaire
+e.preventDefault(); 
+
+ // recuperer toutes les informations 
+ let prenom = document.querySelector("#Prenom").value; 
+ let nom = document.querySelector("#Nom").value; 
+ let email = document.querySelector("#email").value;
+ let message = document.querySelector("#message").value;}
+
+//Date de mise à jour
+
+
